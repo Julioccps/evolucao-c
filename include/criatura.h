fcdb36afa666035b05b2d7e9c1caf7fe1e9fc316
YuriGabrielDev <yurigabriellndr@gmail.com>
2026-04-09 19:37:36 -0300

    Feat: Estrutura criatura criada

--- include/criatura.h
+++ include/criatura.h
@@ -1,7 +1,16 @@
+#include <stdint.h>
+
 #ifndef CRIATURA_H
 #define CRIATURA_H
 
-//mudanca
+typedef struct{
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint16_t x;
+    uint16_t y;
+    uint16_t i_proximo;
+} criatura_t;
 
 void imprimir_c(void);
 
