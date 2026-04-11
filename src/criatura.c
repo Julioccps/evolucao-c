bdc50a2c4b1599e148b338a2dd7ccebfd3c3c1cc
YuriGabrielDev <yurigabriellndr@gmail.com>
2026-04-11 17:28:46 -0300

    Simulação de evolução criada

--- src/criatura.c
+++ src/criatura.c
@@ -1,6 +1,8 @@
 #include "criatura.h"
 #include "stdio.h"
 #include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
 posicao_t gerar_posicao_aleatoria() {
 	posicao_t posicao = {
@@ -19,6 +21,10 @@ cor_t gerar_cor_aleatoria() {
 	return cor;
 }
 
+double gerar_valor_aleatorio() {
+	return (double)rand()/ (double)RAND_MAX;
+}
+
 criatura_t criar_criatura(uint16_t indice_proximo) {
 	criatura_t criatura = {
 		gerar_posicao_aleatoria(),
@@ -30,17 +36,21 @@ criatura_t criar_criatura(uint16_t indice_proximo) {
 
 uint16_t *tamanho_maximo;
 criatura_t *criaturas;
+uint16_t *i_criatura_inicial;
 cor_t *cor;
 uint16_t *pilha;
 uint16_t *i_topo;
 
 void iniciar_criaturas(uint16_t tamanho) {
+	srand(time(NULL));
 	tamanho_maximo = malloc(sizeof(uint16_t));
 	*tamanho_maximo = tamanho;
 	criaturas = malloc(tamanho * sizeof(criatura_t));
 	for (int i = 0; i < tamanho; i++) {
 		criaturas[i] = criar_criatura(i + 1);
 	}
+	i_criatura_inicial = malloc(sizeof(uint16_t));
+	*i_criatura_inicial = 0;
 	cor = malloc(sizeof(cor_t));
 	*cor = gerar_cor_aleatoria();
 	pilha = malloc(tamanho * sizeof(uint16_t));
@@ -48,8 +58,96 @@ void iniciar_criaturas(uint16_t tamanho) {
 	*i_topo = 0;
 };
 
+double calcular_probabilidade_morte(cor_t cor1, cor_t cor2) {
+	double dr = (double)cor1.r - (double)cor2.r;
+    double dg = (double)cor1.g - (double)cor2.g;
+    double db = (double)cor1.b - (double)cor2.b;
+    
+    double distancia = sqrt(dr * dr + dg * dg + db * db);
+    
+    return distancia / 441.6729;
+}
+
+void remover_criatura(uint16_t i, uint16_t i_anterior) {
+	if (i == *i_criatura_inicial) {
+		*i_criatura_inicial = criaturas[i].indice_proximo;	
+	} else {
+		criaturas[i_anterior].indice_proximo = criaturas[i].indice_proximo;
+	}
+	pilha[*i_topo++] = i;
+}
+
+int clamp(int valor, int min, int max) {
+	int valor_final;
+	if (valor < min) {
+		valor_final = min;
+	} else {
+		if (valor > max) {
+			valor_final = max;
+		} else {
+			valor_final = valor;
+		}
+	}
+	return valor_final;
+}
+
+// Já que não posso usar float em valor de cores, vou ter que brincar aqui...
+int gerar_valor_aleatorio_gaussiano() {
+	int valores[10] = {0, 0, 0, 0, 0, 0, 0, 1, 1, 2};
+	int i = rand()%9;
+	int valor_aleatorio;
+	if (rand()%1) {
+		valor_aleatorio = valores[i];
+	} else {
+		valor_aleatorio = -valores[i];
+	}
+	return valor_aleatorio;
+}
+
+void reproduzir_criatura(criatura_t criatura_pai) {
+	cor_t cor_pai = criatura_pai.cor;
+	posicao_t posicao_pai = criatura_pai.posicao;
+	criatura_t criatura_filho = {
+		{
+			clamp(posicao_pai.x + gerar_valor_aleatorio_gaussiano(), 0, 1000),
+			clamp(posicao_pai.y + gerar_valor_aleatorio_gaussiano(), 0, 600),
+		},
+		{
+			clamp(cor_pai.r + gerar_valor_aleatorio_gaussiano(), 0, 255),
+			clamp(cor_pai.g + gerar_valor_aleatorio_gaussiano(), 0, 255),
+			clamp(cor_pai.b + gerar_valor_aleatorio_gaussiano(), 0, 255)
+		},
+		*i_criatura_inicial
+	};
+	uint16_t i_gravacao = pilha[--*i_topo];
+	criaturas[i_gravacao] = criatura_filho;
+	*i_criatura_inicial = i_gravacao;
+}
+
+void reproduzir_criaturas(uint16_t i_criatura) {
+	if (*i_topo > 0) {
+		for (int j = 0; j < (1 + rand()%2); j++) {
+			reproduzir_criatura(criaturas[i_criatura]);	
+		}
+	}
+}
+
 void processar_criaturas() {
-	
+	uint16_t i_anterior = *i_criatura_inicial;
+	uint16_t i = *i_criatura_inicial;
+	while (i != *tamanho_maximo) {
+		cor_t cor_criatura = criaturas[i].cor;
+		double probabilidade_morte = calcular_probabilidade_morte(*cor, cor_criatura);
+		printf("%f\n", gerar_valor_aleatorio());
+		if (gerar_valor_aleatorio() < probabilidade_morte) {
+			printf("criatura removida\n");
+			remover_criatura(i, i_anterior);
+		} else {
+			printf("criatura reproduzida\n");
+			reproduzir_criaturas(i);
+		}
+		i = criaturas[i].indice_proximo;
+	}
 }
 
 void processar() {
@@ -62,12 +160,4 @@ criatura_t* obter_criaturas() {
 
 cor_t* obter_cor_ambiente() {
 	return cor;
-}
-
-void finalizar_criaturas() {
-	free(criaturas);
-	free(pilha);
-	free(cor);
-	free(tamanho_maximo);
-	free(i_topo);
 }
--- src/main.c
+++ src/main.c
@@ -3,17 +3,32 @@
 #include "criatura.h"
 #include "grafico.h"
 
-int main(){
-	iniciar_criaturas(10);
-	
-	while (1){
-		processar();
-		atualizar_interface(
-			obter_criaturas(),
-			obter_cor_ambiente()
-		);
+//Funções temporárias de log, remover quando a interface estiver finalmente pronta
+void mostrar_cor(cor_t cor) {
+	printf("vermelho = %d \n verde = %d \n azul = %d \n", cor.r, cor.g, cor.b);
+}
+
+void mostrar_criaturas(criatura_t *criaturas, int quantidade) {
+	for (int i = 0; i < quantidade; i++) {
+		printf("Cor criatura %d:\n", i);
+		mostrar_cor(criaturas[i].cor);
 	}
+}
+
+int main(){
+	iniciar_criaturas(1000);
 	
-	finalizar_criaturas();
+	cor_t *cor_ambiente = obter_cor_ambiente(); 
+	printf("Cor ambiente:\n");
+	mostrar_cor(*cor_ambiente);
+
+	int rodadas = 100000;
+	while (rodadas-- > 0){
+		processar();
+		// Adicionar função de interface aqui. use funções de criatura.h para obter ponteiros de cor e criaturas
+	}
+
+	mostrar_criaturas(obter_criaturas(), 10);
+	
 	return 0;
 }
